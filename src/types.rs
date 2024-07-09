65968ccded1358cb4659d597c311ff40c3f3550e
Morgan Metz <morgan.metz@eyekon.xyz>
2024-07-09 01:08:52 -0400

    minor update 2

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::fs::read_to_string;
 use anyhow::Result;
+use dirs::home_dir;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
@@ -22,22 +23,32 @@ impl Default for Config {
 impl Config {
     pub fn read_config() -> Result<Config> {
         // Read the config file
-        let config = read_to_string("~/mkprj/config.json")?;
+        let home = home_dir()
+            .expect("Failed to get home directory")
+            .to_str()
+            .unwrap()
+            .to_owned();
+        let config = read_to_string(home + "/.mkprj/config.json")?;
         // Parse the config file
         let config: Config = serde_json::from_str(&config)?;
         Ok(config)
     }
     
     pub fn write_config(self: Self) -> Result<()> {
+        let home = home_dir()
+            .expect("Failed to get home directory")
+            .to_str()
+            .unwrap()
+            .to_owned();
         // Serialize the config
         let config = serde_json::to_string(&self)?;
         // Check if the config directory exists
-        match std::fs::read_dir("~/.mkprj/") {
+        match std::fs::read_dir(home.clone() + "/.mkprj/") {
             Ok(_) => {},
-            Err(_) => std::fs::create_dir("~/.mkprj/").expect("Failed to create config directory"),
+            Err(_) => std::fs::create_dir(home.clone() + "/.mkprj/").expect("Failed to create config directory"),
         }
         // Write the config to the file
-        std::fs::write("~/.mkprj/config.json", config)?;
+        std::fs::write(home + "/.mkprj/config.json", config)?;
         Ok(())
     }
 }
